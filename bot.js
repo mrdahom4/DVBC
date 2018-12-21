8917bede7a403d6a026005295e173785182908df
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-12-21 16:20:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,15 +26,142 @@ client.on('ready', () => {
   console.log('')
 });
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    invites[member.guild.id] = guildInvites;
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "simo");
-    logChannel.send(`Invited by: <@${inviter.id}>`);
-  });
+client.on('ready', () => {
+  client.user.setGame('*bhelp','watching');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
+});
+	
+
+
+	
+
+
+
+client.on('message', message => {
+    if (message.author.bot) return;
+     if (message.content ==="*bhelpar") {
+		 message.channel.send('**تم ارسالك في الخاص**');
+            
+	
+		 
+
+
+ message.author.sendMessage(`**طرق ارسال البروكاست
+ 1-بروكاست لجميع اعضاء السرفر | *bc<الرساله>
+ 2-بروكاست لاعضاء الاونلين فقط  | *bc1<الرساله>
+ 3-بروكاست بامبيد             | *bc3<الرساله>
+ ==================================================================
+
+link Bot : https://discordapp.com/api/oauth2/authorize?client_id=525663482817806357&permissions=0&scope=bot
+
+Server Support : https://discord.gg/BzVpKJB
+
+==================================================================
+ **`);
+ 
+
+    }
+});
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc1")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+
+});
+
+client.on('message', message => {
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == '*bc3') {
+        if (!args[1]) {
+    message.channel.send("**.bc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .setAuthor(message.author.username, message.author.avatarURL)
+                .addField('** الـسيرفر**', `${message.guild.name}`,true)
+                .addField(' **الـمرسل **', `${message.author.username}#${message.author.discriminator}`,true)
+                .addField(' **الرسالة** ', args)
+                .setThumbnail(message.guild.iconURL)
+                .setColor('RANDOM')
+                m.send(`${m}`,{embed: bc});
+            });
+            const AziRo = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)   
+            .setTitle('✔️ | جاري ارسال رسالتك ') 
+            .addBlankField(true)
+            .addField('👥 | عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
+            .addField('📋| الرسالة ', args)
+            .setColor('RANDOM')  
+            message.channel.sendEmbed(AziRo);          
+        }
+        } else {
+            return;
+        }
+});
+
+client.on('message', message => {
+    if (message.author.bot) return;
+     if (message.content === "*bhelp") {
+	    
+                        message.channel.send('**Choose**: *bhelpar ➾ arabic | *bhelpen ➾ english');
+
+    }
+});
+
+client.on('message', message => {
+    if (message.author.bot) return;
+     if (message.content === "*bhelpen") {
+		 message.channel.send('**The Message Was Sent On Private**');
+            
+	
+		 
+
+
+ message.author.sendMessage(`**Ways to send a Bodcast
+1-Procast for all server members  |*bc<the message>
+2-Procast for only Online members |*bc1<the message>
+3-Brocade Pamped                  |*bc2<the message>
+
+==================================================================
+
+link Bot : https://discordapp.com/api/oauth2/authorize?client_id=525663482817806357&permissions=0&scope=bot
+
+Server Support : https://discord.gg/BzVpKJB
+
+==================================================================
+**`);
+
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
